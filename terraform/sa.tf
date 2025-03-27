d7e525fa43910de563f1ffe9ab857bf70ad89666
Amit Purohit <amitpurohit4747@gmail.com>
2025-03-27 16:25:59 +0530

    Add BigQuery service account resource to Terraform configuration

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -4,3 +4,11 @@ resource "google_service_account" "cloudrun-poc-sa" {
   project = var.project_id
   description = "Service account for the Cloud Run PoC"
 }
+
+resource "google_service_account" "bq-sa" {
+  account_id = "bq-sa"
+  display_name = "BigQuery Service Account"
+  project = var.project_id
+  description = "Service account for accessing BigQuery"
+}
+
