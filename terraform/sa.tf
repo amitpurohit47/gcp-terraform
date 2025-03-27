748b21097cd592e0530b5f8d99112cbcd1ca3948
Amit Purohit <amitpurohit4747@gmail.com>
2025-03-27 16:35:43 +0530

    Rename BigQuery service account account_id for consistency

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -6,7 +6,7 @@ resource "google_service_account" "cloudrun-poc-sa" {
 }
 
 resource "google_service_account" "bigquery-sa" {
-  account_id = "bq-sa"
+  account_id = "bigquery-sa"
   display_name = "BigQuery Service Account"
   project = var.project_id
   description = "Service account for accessing BigQuery"
