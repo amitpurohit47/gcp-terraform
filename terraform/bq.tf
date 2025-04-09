3fbe03525cb43d44a989d2683303e734a6f66bd3
Amit Purohit <amitpurohit4747@gmail.com>
2025-04-09 08:24:09 +0530

    Added BigQuery notification dataset and table resources with schema definition

---
+++ terraform/bq.tf
@@ -0,0 +1,13 @@
+resource "google_bigquery_dataset" "notification_dataset" {
+    dataset_id = "notification_dataset"
+    project    = var.project_id
+    location   = "US"
+}
+
+resource "google_bigquery_table" "notification_table" {
+  dataset_id = google_bigquery_dataset.notification_dataset.dataset_id
+  table_id = "notification_table"
+  project = var.project_id
+  schema = file("/schema/notification_table_schema.json")
+}
+  
